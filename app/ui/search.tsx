218724fd4a33053f7122a59933c3f15d3239ae43
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-07 19:47:39 +0000

    Add search functionality to UI component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,23 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
+    console.log(`Searching... ${term}`)
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -26,6 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
