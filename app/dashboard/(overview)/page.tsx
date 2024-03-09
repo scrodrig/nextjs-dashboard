3ffd9769f06631aadb7f7788ee012b4708fba3c6
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-09 20:17:42 +0000

    Add metadata for dashboard page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,10 +6,15 @@ import {
 
 import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Metadata } from 'next';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { Suspense } from 'react';
 import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   const {
